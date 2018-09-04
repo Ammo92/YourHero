9c8c904667c51a43d06ff47e063cf2eb73cb16cd
GULJINDER Karan <KGULJINDER@fr.scc.com>
2018-09-04 16:26:51 +0200

    Add MainActivity

--- app/src/main/java/com/example/mohamed/yourhero/MainActivity.java
+++ app/src/main/java/com/example/mohamed/yourhero/MainActivity.java
@@ -1,13 +1,57 @@
 package com.example.mohamed.yourhero;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        TextView appTitle = findViewById(R.id.apptitle);
+        Button buttonPlay = findViewById(R.id.buttonPlay);
+        Button buttonHistoric = findViewById(R.id.buttonHistoric);
+        Button buttonExit = findViewById(R.id.buttonExit);
+
+
+        setFont(appTitle,"SHOWG.TTF");
+        setFontButton(buttonPlay,"SHOWG.TTF");
+        setFontButton(buttonHistoric,"SHOWG.TTF");
+        setFontButton(buttonExit,"SHOWG.TTF");
+
+    }
+
+    public void setFont(TextView textView, String fontName) {
+        if(fontName != null){
+            try {
+                Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/" + fontName);
+                textView.setTypeface(typeface);
+            } catch (Exception e) {
+                Log.e("FONT", fontName + " not found", e);
+            }
+        }
+    }
+
+    public void setFontButton(Button button, String fontName) {
+        if(fontName != null){
+            try {
+                Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/" + fontName);
+                button.setTypeface(typeface);
+            } catch (Exception e) {
+                Log.e("FONT", fontName + " not found", e);
+            }
+        }
     }
 }
