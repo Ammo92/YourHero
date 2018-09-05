10fad734206fd6f720f95501aab3d91f4ca2cf70
GULJINDER Karan <KGULJINDER@fr.scc.com>
2018-09-05 16:36:42 +0200

    Add Hero Result

--- app/src/main/java/com/example/mohamed/yourhero/HeroActivity.java
+++ app/src/main/java/com/example/mohamed/yourhero/HeroActivity.java
@@ -1,13 +1,41 @@
 package com.example.mohamed.yourhero;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class HeroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_hero);
+
+        TextView heroName = findViewById(R.id.heroName);
+        TextView heroAlias = findViewById(R.id.heroAlias);
+        TextView heroTeam = findViewById(R.id.heroTeam);
+
+        setFont(heroName, "SHOWG.TTF");
+        setFont(heroAlias, "SHOWG.TTF");
+        setFont(heroTeam, "SHOWG.TTF");
+
+    }
+
+    public void setFont(TextView textView, String fontName) {
+        if (fontName != null) {
+            try {
+                Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/" + fontName);
+                textView.setTypeface(typeface);
+            } catch (Exception e) {
+                Log.e("FONT", fontName + " not found", e);
+            }
+
+        }
     }
 }
